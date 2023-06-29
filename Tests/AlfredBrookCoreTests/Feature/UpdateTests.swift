e13f486bdb0edd68e517336b71dc1d6da0f17962
G <guill.bout@gmail.com>
2023-06-29 21:40:45 +0800

    start updating a couple of tests

--- Tests/AlfredBrookCoreTests/Feature/UpdateTests.swift
+++ Tests/AlfredBrookCoreTests/Feature/UpdateTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 
-class UpdateTests: AlfredKatTestCase {
+class UpdateTests: AlfredBrookTestCase {
     
     func test_that_if_there_is_an_updateAvailable_file_in_the_Workflow_cache_folder_then_we_show_the_update_in_Alfred_Results() throws {
         try? Self.mockAlreadyCreatedReleaseInfoFile()
